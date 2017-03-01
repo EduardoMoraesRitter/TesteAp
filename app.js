273262c232d223587ff6f2d49d90c85a17cd96df
Eduardo Ritter <eduardo.moraes.ritter@gmail.com>
2017-03-01 10:30:03 -0300

    teste7

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-var express = require('express'), 
-app = express();
-//app.use(express.logger());
+var express = require('express');
+var http = require('http');
+var app = express();
+
 app.get('/', function(req, res){
     res.send('Ola Mundo');
 });
-module.exports = app;
+
+var server = http.createServer(app);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+var port = normalizePort(process.env.PORT || '3000');
+server.listen(port);
+
+console.log('Servidor Express iniciado na porta %s', server.address().port);
